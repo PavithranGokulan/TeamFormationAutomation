ea10d17d25661d25c99f09001d10a90492777cb3
Pavithran G <92795317+PavithranGokulan@users.noreply.github.com>
2025-06-08 00:03:25 +0530

    Update Login.jsx

--- Frontend/src/authentication/Login.jsx
+++ Frontend/src/authentication/Login.jsx
@@ -29,7 +29,7 @@ export default function Login() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://teamformationautomation-backend.onrender.com/api/auth/login",
         formData
       );
       if (response.status == 200) {
